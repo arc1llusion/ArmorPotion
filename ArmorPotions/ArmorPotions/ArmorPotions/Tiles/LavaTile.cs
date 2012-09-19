2c8465c3be432ecec641d3c7ecc67729489b5320
John P Close <JohnPClose@gmail.com>
2012-09-19 02:56:10 -0400

    Water Tile Lava Tile Logic Update

--- ArmorPotions/ArmorPotions/ArmorPotions/Tiles/LavaTile.cs
+++ ArmorPotions/ArmorPotions/ArmorPotions/Tiles/LavaTile.cs
@@ -9,14 +9,38 @@ namespace ArmorPotions.Tiles
 {
     class LavaTile : Tile
     {
+        bool _isCooled;
+        Texture2D _lavaTexture;
+        Texture2D _cooledLavaTexture;
         public LavaTile(TileType tileType, int tileID, Texture2D lavaTexture, Texture2D cooledLavaTexture, bool isCooled)
             : base(tileType, tileID, lavaTexture)
         {
+            _isCooled = isCooled;
+            _lavaTexture = lavaTexture;
+            _cooledLavaTexture = cooledLavaTexture;
+            if (_isCooled)
+            {
+                Texture = _cooledLavaTexture;
+                TileType = TileType.Passable;
+            }
+            else{
+                Texture = _lavaTexture;
+                TileType = TileType.Hole;
+            }
         }
-        bool _isCooled;
 
         public override void onEvent(EventType sendEvent)
         {
+
+            if(sendEvent == EventType.IceEvent && !_isCooled){
+                _isCooled = true;
+                Texture = _cooledLavaTexture;
+                TileType = TileType.Passable;
+            }else if(sendEvent == EventType.FireEvent && _isCooled){
+                _isCooled = false;
+                Texture = _lavaTexture;
+                TileType = TileType.Hole;
+            }
             throw new NotImplementedException();
         }
     }
--- ArmorPotions/ArmorPotions/ArmorPotions/Tiles/WaterTile.cs
+++ ArmorPotions/ArmorPotions/ArmorPotions/Tiles/WaterTile.cs
@@ -11,13 +11,29 @@ namespace ArmorPotions.Tiles
     {
         bool _isDeep;
         bool _isFrozen;
+        Texture2D _waterTexture;
+        Texture2D _deepWaterTexture;
+        Texture2D _iceTexture;
 
-        public WaterTile(TileType tileType, int tileID, Texture2D waterTexture, Texture2D iceTexture, bool isFrozen)
+        public WaterTile(TileType tileType, int tileID, Texture2D waterTexture, Texture2D iceTexture, Texture2D deepWaterTexture, bool isDeep, bool isFrozen)
             : base(tileType, tileID, waterTexture)
         {
-            if (isFrozen)
+            _isDeep = isDeep;
+            _isFrozen = isFrozen;
+            if (_isFrozen)
             {
                 Texture = iceTexture;
+                TileType = TileType.Passable;
+            }
+            else if (_isDeep)
+            {
+                Texture = deepWaterTexture;
+                TileType = TileType.Hole;
+            }
+            else
+            {
+                Texture = waterTexture;
+                TileType = TileType.Passable;
             }
 
         }
@@ -27,8 +43,26 @@ namespace ArmorPotions.Tiles
             if (sendEvent == EventType.IceEvent && !_isFrozen)
             {
                 _isFrozen = true;
-            }else if(sendEvent == EventType.FireEvent && _isFrozen){
+                Texture = _iceTexture;
+                TileType = TileType.Passable;
+            }
+            else if (sendEvent == EventType.FireEvent && _isFrozen)
+            {
                 _isFrozen = false;
+                if (_isDeep)
+                {
+                    Texture = _deepWaterTexture;
+                    TileType = TileType.Hole;
+                }
+                else
+                {
+                    Texture = _waterTexture;
+                    TileType = TileType.Passable;
+                }
+            }
+            else
+            {
+                Texture = _waterTexture;
             }
 
             throw new NotImplementedException();
