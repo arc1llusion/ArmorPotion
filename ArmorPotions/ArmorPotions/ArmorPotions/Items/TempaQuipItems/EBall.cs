30c04cc54480c8f25f0a548995a396883a23c0e3
surfintime0027 <surfintime0027@hotmail.com>
2012-09-19 05:37:48 -0400

    Fixed E-Ball Angle
    Also added constructor values to modify e-ball behavior

--- ArmorPotions/ArmorPotions/ArmorPotions/Items/TempaQuipItems/EBall.cs
+++ ArmorPotions/ArmorPotions/ArmorPotions/Items/TempaQuipItems/EBall.cs
@@ -6,20 +6,32 @@ using ArmorPotionFramework.Items;
 using Microsoft.Xna.Framework.Graphics;
 using ArmorPotionFramework.SpriteClasses;
 using ArmorPotionFramework.Projectiles;
+using Microsoft.Xna.Framework;
 
 namespace ArmorPotions.Items.TempaQuipItems
 {
     public class EBall : TempaQuip
     {
         private AnimatedSprite _secondaryProjectileSprite;
+        private float _throwDistance;
+        private float _spreadAngle;
+        private float _revolutions;
+        private float _projectilesPerIteration;
+        private float _projectileDistance;
 
-        public EBall(Texture2D icon, String name, AnimatedSprite sprite, int wait, AnimatedSprite secondaryProjectileSprite)
+        public EBall(Texture2D icon, String name, AnimatedSprite sprite, int wait, AnimatedSprite secondaryProjectileSprite, float throwDistance, float projectileDistance, int spreadAngle, float revolutions, float projectilesPerIteration)
             : base(icon, name, sprite, wait)
         {
             _secondaryProjectileSprite = secondaryProjectileSprite;
 
             this._allowMulti = false;
             this._hasProjectile = false;
+
+            _throwDistance = throwDistance;
+            _projectileDistance = projectileDistance;
+            _spreadAngle = MathHelper.ToRadians(spreadAngle);
+            _revolutions = revolutions;
+            _projectilesPerIteration = projectilesPerIteration;
         }
 
         public override void OnEquip(ArmorPotionFramework.EntityClasses.Player equippedBy)
@@ -32,7 +44,16 @@ namespace ArmorPotions.Items.TempaQuipItems
             {
                 _currentWaitTime = _maxWaitTime;
 
-                ThrowProjectile projectile = new ThrowProjectile(activatedBy.World, this, this.CenterEntity(activatedBy), 100, 270, 75, Math.PI / 4, 2, 1);
+                ThrowProjectile projectile = new ThrowProjectile(
+                    activatedBy.World, 
+                    this, this.CenterEntity(activatedBy), 
+                    _throwDistance, 
+                    MathHelper.ToRadians((int)activatedBy.CurrentSprite.CurrentAnimation * 90), 
+                    _projectileDistance, 
+                    _spreadAngle, 
+                    _revolutions, 
+                    _projectilesPerIteration);
+
                 projectile.AnimatedSprites.Add("Normal", AnimatedSprite);
                 projectile.AnimatedSprites.Add("Projectile", _secondaryProjectileSprite);
 
